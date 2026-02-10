1f21add6d7bab3f72b6de015188694565c82d47e
3wguap <rnbrell@ya.ru>
2026-02-10 18:33:05 +0400

    Создать сущности в слое model

---
+++ src/main/java/blog/model/Comment.java
@@ -0,0 +1,28 @@
+package blog.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+    @Column(nullable = false)
+    private String text;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
